b83853dea484acc19db4a377dc583be7c4a2d762
StoneJiaJia <huyou1130@163.com>
2017-02-28 23:58:20 +0800

    Add

---
+++ Chapter1/1.12.scm
@@ -0,0 +1,8 @@
+(define (triangle row col)
+  (cond ((> col row) 0)
+        ((< col 0) 0)
+        ((= col 1) 1)
+        ((+ (triangle (- row 1) (- col 1))
+            (triangle (- row 1) col)))))
+
+(triangle 5 2)
