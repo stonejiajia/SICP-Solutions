ded4d5e227a82f17f4f8f23729391aeb79d31b03
石子佳 <stone20091652@ishikoyotekiMacBook-Pro.local>
2018-01-05 14:38:44 +0800

    test

---
+++ elisp/.custom-settings.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (treemacs realgud pyvenv alert org-brain markdown-mode live-py-mode js2-mode ibuffer-projectile hindent evil-org with-editor dante company cmake-ide cider cargo yasnippet pythonic ivy zenburn-theme yapfify xterm-color ws-butler winum which-key wgrep web-mode web-beautify volatile-highlights virtualenvwrapper vi-tilde-fringe uuidgen use-package unfill toml-mode toc-org test-simple tern tagedit symon string-inflection spaceline-all-the-icons solarized-theme smex smeargle slim-mode shell-pop scss-mode sass-mode restart-emacs request ranger rainbow-delimiters racer queue pytest py-isort pug-mode pretty-mode prettify-utils popwin pip-requirements pfuture persp-mode peg pdf-tools pcre2el password-generator paradox outshine org-projectile org-present org-download org-bullets open-junk-file nord-theme neotree mwim multi-term move-text mmm-mode macrostep lorem-ipsum log4e loc-changes load-relative linum-relative link-hint levenshtein less-css-mode json-mode js-doc info+ indent-guide impatient-mode hungry-delete hlint-refactor hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-make helm-descbinds helm-ag graphviz-dot-mode google-translate golden-ratio gnuplot gntp gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe ghc gh-md fuzzy flycheck-rust flycheck-pos-tip flycheck-haskell flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav editorconfig dumb-jump disaster diminish diff-hl define-word dash-functional cython-mode csv-mode column-enforce-mode coffee-mode cmm-mode cmake-mode clojure-mode clean-aindent-mode clang-format cider-eval-sexp-fu browse-at-remote auto-highlight-symbol auto-compile all-the-icons-dired aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
