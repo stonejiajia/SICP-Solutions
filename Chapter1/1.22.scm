6c086398ec7c90454f63cf3f295a500d610c8691
StoneJiaJia <huyou1130@163.com>
2018-01-09 11:08:21 +0800

    Add: Change 1.22 exercise

--- Chapter1/1.22.scm
+++ Chapter1/1.22.scm
@@ -1,16 +1,48 @@
 
+(define (square x) (* x x))
 
-;;
-(define (smallest-divisor n)
-  (find-divisor n 2))
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (next-divisor test-divisor)))))
-(define (divides? a b)
-  (= (remainder b a) 0))
+(define (next-odd n)
+  (if (odd? n)
+      (+ 2 n)
+      (+ 1 n)))
+
 (define (prime? n)
-  (= n (smallest-divisor n)))
-(define (next-divisor n)
-  (if (= n 2) 3
-      (+ n 2)))
+    (= n (smallest-divisor n)))
+
+
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n)
+            n)
+          ((divides? test-divisor n)
+            test-divisor)
+          (else
+            (find-divisor n (+ test-divisor 1)))))
+
+
+(define (divides? a b)
+    (= (remainder b a) 0))
+
+
+(define (continue-primes n count)
+    (cond ((= count 0)
+            (display "are primes."))
+          ((prime? n)
+            (display n)
+            (newline)
+            (continue-primes (next-odd n) (- count 1)))
+          (else
+            (continue-primes (next-odd n) count))))
+
+
+(prime? 1999)
+
+(continue-primes 100 2
+                 )
+
+(time (continue-primes 100 2
+                       ))
