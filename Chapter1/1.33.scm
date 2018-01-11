1faed2769ccb0c1057c36f94a06aada294a0c11a
StoneJiaJia <huyou1130@163.com>
2018-01-11 21:11:23 +0800

    Add: Add 1.38 exercise

--- Chapter1/1.33.scm
+++ Chapter1/1.33.scm
@@ -1,4 +1,23 @@
 
+(define (sq x) (* x x))
+
+
+
+(define (smallest-div n) 
+    (define (divides? a b) 
+      (= 0 (remainder b a))) 
+    (define (find-div n test) 
+       (cond ((> (sq test) n) n) ((divides? test n) test) 
+             (else (find-div n (+ test 1))))) 
+    (find-div n 2)) 
+  
+(define (prime? n) 
+  (if (= n 1) false (= n (smallest-div n)))) 
+
+
+
+
+
 
 
 (define (filtered-accumulate combiner null-value term a next b filter) 
@@ -8,5 +27,8 @@
                     (filtered-accumulate combiner null-value term (next a) next b filter)) 
           (combiner null-value 
                     (filtered-accumulate combiner null-value term (next a) next b filter))))) 
+
+
+
 
 
